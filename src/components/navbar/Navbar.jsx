148a6a8b848a8c1261661699566711b8165e2aed
Santosh Kumar <mrsantoshkr1985@gmail.com>
2024-01-23 19:21:08 +0530

    added styles in project not complete though

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,9 @@
-import Link from "next/link";
 import Links from "./links/Links";
+import styles from "./navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>Logo</div>
       <div>
         <Links />
